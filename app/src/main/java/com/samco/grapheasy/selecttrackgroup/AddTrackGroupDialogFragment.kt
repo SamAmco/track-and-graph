bd4b2b4306317563ec91a4f1ad2c80a0e8babe01
Samuel Amantea-Collins <samuel.amco@gmail.com>
2019-09-05 18:01:31 +0100

    stopped allowing adding of track groups with empty name.

--- app/src/main/java/com/samco/grapheasy/selecttrackgroup/AddTrackGroupDialogFragment.kt
+++ app/src/main/java/com/samco/grapheasy/selecttrackgroup/AddTrackGroupDialogFragment.kt
@@ -4,15 +4,18 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.WindowManager
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import com.samco.grapheasy.R
 
 
-class AddTrackGroupDialogFragment : DialogFragment() {
+class AddTrackGroupDialogFragment : DialogFragment(), TextWatcher {
     private lateinit var listener: AddTrackGroupDialogListener
     private lateinit var editText: EditText
+    private lateinit var alertDialog: AlertDialog
 
     interface AddTrackGroupDialogListener {
         fun onAddTrackGroup(name: String)
@@ -32,17 +35,30 @@ class AddTrackGroupDialogFragment : DialogFragment() {
         return activity?.let {
             val view = it.layoutInflater.inflate(R.layout.add_track_group_dialog, null)
             editText = view.findViewById(R.id.track_group_name_input)
+            editText.addTextChangedListener(this)
             var builder = AlertDialog.Builder(it)
-            //TODO some validation of the name
             builder.setView(view)
                 .setPositiveButton(R.string.add) { _, _ -> listener.onAddTrackGroup(editText.text.toString()) }
                 .setNegativeButton(R.string.cancel) { _, _ -> {} }
-            builder.create()
+            alertDialog = builder.create()
+            alertDialog
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 
+    override fun afterTextChanged(p0: Editable?) { }
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, length: Int) {
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = length > 0
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
         super.onActivityCreated(savedInstanceState)
+        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
     }
 }
