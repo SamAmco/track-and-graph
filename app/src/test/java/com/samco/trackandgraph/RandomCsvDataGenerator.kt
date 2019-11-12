f02c33e909f8de3bce78bd2cebdc604c339efdf6
Samuel Amantea-Collins <samuel.amco@gmail.com>
2019-11-12 23:01:35 +0000

    created a random correlated data generator for demo purposes.

--- app/src/test/java/com/samco/trackandgraph/RandomCsvDataGenerator.kt
+++ app/src/test/java/com/samco/trackandgraph/RandomCsvDataGenerator.kt
@@ -7,6 +7,8 @@ import org.junit.Test
 import org.threeten.bp.OffsetDateTime
 import java.io.File
 import java.io.PrintWriter
+import kotlin.math.roundToInt
+import kotlin.math.sin
 import kotlin.random.Random
 
 /**
@@ -33,9 +35,9 @@ class RandomCsvDataGenerator {
     }
 
     @Test
-    fun addition_isCorrect() {
+    fun generate_random_data() {
         val numRecords = 1000000
-        PrintWriter(File("./$numRecords-test.txt")).use {
+        PrintWriter(File("./$numRecords-test.csv")).use {
             it.println(CSVReadWriter.HEADERS.values().joinToString(",") { h -> h.name } )
             var currTime = OffsetDateTime.now()
             for (i in 0..numRecords) {
@@ -45,4 +47,98 @@ class RandomCsvDataGenerator {
             }
         }
     }
+
+    private class SinTransform(val amplitude: Double, val wavelength: Double) {
+        fun transform(index: Int): Double {
+            val sinTransform = sin((index.toDouble() / wavelength) * Math.PI * 2.0)
+            return ((sinTransform + 1.0) / 2.0) * amplitude
+        }
+    }
+
+    private val wpt1 = SinTransform(1.0, 3.0)
+    private val wpt2 = SinTransform(0.5, 21.0)
+    private val wpt3 = SinTransform(0.25, 90.0)
+    private val wpTotalAmp = 1.75
+    private val randomRatio = 0.5
+    private val sinRatio = 0.5
+
+    private fun generateWheatPainData_Wheat(index: Int): LineData {
+        val sinTransform = (wpt1.transform(index) + wpt2.transform(index) + wpt3.transform(index)) / wpTotalAmp
+        val randAdjusted = (sinTransform * sinRatio) + (Random.nextDouble() * randomRatio)
+        val value = (randAdjusted * 2.0).roundToInt()
+        println("$sinTransform -> $value")
+        val valueString = when (value) {
+            0 -> "0:None"
+            1 -> "1:Some"
+            2 -> "2:Lots"
+            else -> throw Exception("unrecognised int generated")
+        }
+        return LineData("Wheat", valueString)
+    }
+
+    private fun generateWheatPainData_Pain(index: Int): LineData {
+        val sinTransform = (wpt1.transform(index) + wpt2.transform(index) + wpt3.transform(index)) / wpTotalAmp
+        val randAdjusted = (sinTransform * sinRatio) + (Random.nextDouble() * randomRatio)
+        val valueString = (randAdjusted * 10.0).roundToInt().toString()
+        return LineData("Stomach Pain", valueString)
+    }
+
+    private fun printLineData(lineData: List<LineData>, currTime: OffsetDateTime, printWriter: PrintWriter) {
+        lineData.forEach { lineData ->
+            printWriter.println("${lineData.featureName},${databaseFormatter.format(currTime)},${lineData.featureValue}")
+        }
+    }
+
+    @Test
+    fun generate_wheat_pain_correlated_data() {
+        val numRecords = 100
+        PrintWriter(File("./wheat-pain-test.csv")).use {
+            it.println(CSVReadWriter.HEADERS.values().joinToString(",") { h -> h.name } )
+            var currTime = OffsetDateTime.now()
+                .withHour(0)
+                .withMinute(0)
+                .withSecond(0)
+                .withNano(0)
+                .minusDays(numRecords / 3L)
+            for (x in 0..(numRecords / 3)) {
+                var index = x * 3
+                currTime = currTime.withHour(9)
+                val wheatData1 = generateWheatPainData_Wheat(index)
+                val painData1 = generateWheatPainData_Pain(index)
+                printLineData(listOf(wheatData1, painData1), currTime, it)
+                index++
+
+                currTime = currTime.withHour(13)
+                val wheatData2 = generateWheatPainData_Wheat(index)
+                val painData2 = generateWheatPainData_Pain(index)
+                printLineData(listOf(wheatData2, painData2), currTime, it)
+                index++
+
+                currTime = currTime.withHour(19)
+                val wheatData3 = generateWheatPainData_Wheat(index)
+                val painData3 = generateWheatPainData_Pain(index)
+                printLineData(listOf(wheatData3, painData3), currTime, it)
+
+                currTime = currTime.plusDays(1)
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
