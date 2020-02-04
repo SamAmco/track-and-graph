df24cca13c9ee604fa7929b4ca07e33ea9a64161
Pim Kunis <pim@kunis.nl>
2020-02-04 21:38:23 +0100

    quick and dirty insert of datapoint from widget

--- app/src/main/java/com/samco/trackandgraph/widgets/TrackWidgetProvider.kt
+++ app/src/main/java/com/samco/trackandgraph/widgets/TrackWidgetProvider.kt
@@ -8,6 +8,11 @@ import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
 import com.samco.trackandgraph.R
+import com.samco.trackandgraph.database.DataPoint
+import com.samco.trackandgraph.database.TrackAndGraphDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.threeten.bp.OffsetDateTime
 
 const val TRACK_WIDGET_BUTTON_CLICK = "TRACK_WIDGET_BUTTON_CLICK"
 
@@ -33,6 +38,14 @@ class TrackWidgetProvider : AppWidgetProvider() {
 
         if (intent.action == TRACK_WIDGET_BUTTON_CLICK) {
             Log.d("Widget", "Widget button clicked!")
+            GlobalScope.launch {
+                val dao = TrackAndGraphDatabase.getInstance(context).trackAndGraphDatabaseDao
+                val trackGroups = dao.getAllGroupsSync()
+                val feature = dao.getFeatureById(trackGroups[0].id)
+                val newDataPoint = DataPoint(OffsetDateTime.now(), feature.id, 1.0, "")
+                Log.d("Widget", "inserting datapoint " + newDataPoint.toString())
+                dao.insertDataPoint(newDataPoint)
+            }
         }
     }
 
