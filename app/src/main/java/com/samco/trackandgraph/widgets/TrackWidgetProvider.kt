52e44415c20e71cfdb1023fc38f726f27a2941b6
Pim Kunis <pim@kunis.nl>
2020-02-05 00:35:19 +0100

    Open card fragment to add data point.

--- app/src/main/java/com/samco/trackandgraph/widgets/TrackWidgetProvider.kt
+++ app/src/main/java/com/samco/trackandgraph/widgets/TrackWidgetProvider.kt
@@ -5,16 +5,10 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.widget.RemoteViews
 import com.samco.trackandgraph.R
-import com.samco.trackandgraph.database.DataPoint
-import com.samco.trackandgraph.database.TrackAndGraphDatabase
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.threeten.bp.OffsetDateTime
 
-const val TRACK_WIDGET_BUTTON_CLICK = "TRACK_WIDGET_BUTTON_CLICK"
+const val FEATURE_KEY = "FEATURE_KEY"
 
 class TrackWidgetProvider : AppWidgetProvider() {
     override fun onUpdate(
@@ -26,32 +20,14 @@ class TrackWidgetProvider : AppWidgetProvider() {
 
         appWidgetIds?.forEach { id ->
             val remoteViews = RemoteViews(context.packageName, R.layout.track_widget)
+            val intent = Intent(context, TrackWidgetInputDataPoint::class.java)
+            // Temp id; let the user choose on widget construction in the future.
+            val featureId: Long = 2
+            intent.putExtra(FEATURE_KEY, featureId)
             remoteViews.setOnClickPendingIntent(R.id.track_widget_button,
-                getPendingSelfIntent(context, TRACK_WIDGET_BUTTON_CLICK))
+                PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT))
 
             appWidgetManager.updateAppWidget(id, remoteViews)
         }
-    }
-
-    override fun onReceive(context: Context, intent: Intent) {
-        super.onReceive(context, intent)
-
-        if (intent.action == TRACK_WIDGET_BUTTON_CLICK) {
-            Log.d("Widget", "Widget button clicked!")
-            GlobalScope.launch {
-                val dao = TrackAndGraphDatabase.getInstance(context).trackAndGraphDatabaseDao
-                val trackGroups = dao.getAllGroupsSync()
-                val feature = dao.getFeatureById(trackGroups[0].id)
-                val newDataPoint = DataPoint(OffsetDateTime.now(), feature.id, 1.0, "")
-                Log.d("Widget", "inserting datapoint " + newDataPoint.toString())
-                dao.insertDataPoint(newDataPoint)
-            }
-        }
-    }
-
-    private fun getPendingSelfIntent(context: Context, action: String) : PendingIntent {
-        val intent = Intent(context, javaClass)
-        intent.action = action
-        return PendingIntent.getBroadcast(context, 0, intent, 0)
     }
 }
