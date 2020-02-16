9124beae346b5d90efcea01aec632c7d5539f578
Samuel Harvey-Collins <samuel.amco@gmail.com>
2020-02-16 19:21:30 +0000

    bug fix bad initial value for fixed y range to on database migrations.

--- app/src/main/java/com/samco/trackandgraph/database/TrackAndGraphDatabase.kt
+++ app/src/main/java/com/samco/trackandgraph/database/TrackAndGraphDatabase.kt
@@ -109,7 +109,7 @@ val MIGRATION_30_31 = object : Migration(30, 31) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE line_graphs_table ADD y_range_type INTEGER NOT NULL DEFAULT  0")
         database.execSQL("ALTER TABLE line_graphs_table ADD y_from REAL NOT NULL DEFAULT  0")
-        database.execSQL("ALTER TABLE line_graphs_table ADD y_to REAL NOT NULL DEFAULT  0")
+        database.execSQL("ALTER TABLE line_graphs_table ADD y_to REAL NOT NULL DEFAULT  1")
     }
 }
 
