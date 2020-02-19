10df740130d6765c0949d7624036208c10f59cce
Pim Kunis <pim@kunis.nl>
2020-02-19 16:39:23 +0100

    Try to add spinner in widget configure

--- app/src/main/java/com/samco/trackandgraph/widgets/TrackWidgetConfigure.kt
+++ app/src/main/java/com/samco/trackandgraph/widgets/TrackWidgetConfigure.kt
@@ -5,20 +5,67 @@ import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import com.samco.trackandgraph.R
+import com.samco.trackandgraph.database.FeatureAndTrackGroup
+import com.samco.trackandgraph.database.TrackAndGraphDatabase
+import com.samco.trackandgraph.database.TrackAndGraphDatabaseDao
+import com.samco.trackandgraph.databinding.TrackWidgetConfigureBinding
+import timber.log.Timber
+import java.lang.Exception
 
-class TrackWidgetConfigure : Activity() {
+class TrackWidgetConfigure : FragmentActivity() {
 
     private var appWidgetId: Int? = null
+    private lateinit var viewModel: TrackWidgetConfigureViewModel
+    private lateinit var binding: TrackWidgetConfigureBinding
+    private var featureId: Long? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.track_widget_configure)
 
+        binding = TrackWidgetConfigureBinding.inflate(layoutInflater)
         appWidgetId = intent?.extras?.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID) ?: AppWidgetManager.INVALID_APPWIDGET_ID
+
+
         setResult(RESULT_CANCELED)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        viewModel = ViewModelProviders.of(this).get(TrackWidgetConfigureViewModel::class.java)
+        viewModel.initViewModel(this)
+        viewModel.allFeatures.observe(this, Observer { features ->
+            val itemNames = features.map {ft -> "${ft.trackGroupName} -> ${ft.name}"}
+            Timber.d(itemNames.toString())
+            val adapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, itemNames)
+            binding.featureSpinner.adapter = adapter
+            binding.featureSpinner.setSelection(0)
+            binding.featureSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    featureId = features[position].id
+                }
+            }
+        })
+
+        return null
     }
 
     fun onConfirm(view: View) {
@@ -27,7 +74,7 @@ class TrackWidgetConfigure : Activity() {
                 return
             }
 
-            val featureId: Long = 2
+            val featureId: Long = 2 // temp
             val sharedPref = getSharedPreferences(WIDGET_PREFS_NAME, Context.MODE_PRIVATE).edit()
             sharedPref.putLong(TrackWidgetProvider.getFeatureIdPref(id), featureId)
             sharedPref.apply()
@@ -42,5 +89,16 @@ class TrackWidgetConfigure : Activity() {
             setResult(RESULT_OK, resultValue)
             finish()
         }
+    }
+}
+
+class TrackWidgetConfigureViewModel : ViewModel() {
+    private var dataSource: TrackAndGraphDatabaseDao? = null
+    lateinit var allFeatures: LiveData<List<FeatureAndTrackGroup>> private set
+
+    fun initViewModel(activity: Activity) {
+        if (dataSource != null) return
+        dataSource = TrackAndGraphDatabase.getInstance(activity.application).trackAndGraphDatabaseDao
+        allFeatures = dataSource!!.getAllFeaturesAndTrackGroups()
     }
 }
