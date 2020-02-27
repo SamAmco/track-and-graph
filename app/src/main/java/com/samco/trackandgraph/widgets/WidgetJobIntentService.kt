f300557aba0f7d3fa3f93c27b8e9b08431b82929
Pim Kunis <pim@kunis.nl>
2020-02-27 23:42:48 +0100

    Indicate when widget is disabled.

--- app/src/main/java/com/samco/trackandgraph/widgets/WidgetJobIntentService.kt
+++ app/src/main/java/com/samco/trackandgraph/widgets/WidgetJobIntentService.kt
@@ -3,12 +3,8 @@ package com.samco.trackandgraph.widgets
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
-import android.os.SystemClock
-import android.widget.RemoteViews
 import androidx.core.app.JobIntentService
-import com.samco.trackandgraph.R
 import com.samco.trackandgraph.database.TrackAndGraphDatabase
-import timber.log.Timber
 
 class WidgetJobIntentService : JobIntentService() {
     companion object {
@@ -17,27 +13,16 @@ class WidgetJobIntentService : JobIntentService() {
         }
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        Timber.d("onCreate")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.d("onDestroy")
-    }
-
     override fun onHandleWork(intent: Intent) {
-        Timber.d("onHandleWork")
-
         val appWidgetManager = AppWidgetManager.getInstance(this)
 
         val appWidgetId = intent.getIntExtra("appWidgetIdExtra", -1)
+        val disableWidget = intent.getBooleanExtra("disableWidgetExtra", false)
 
         val featureId = this.getSharedPreferences(WIDGET_PREFS_NAME, Context.MODE_PRIVATE).getLong(
             TrackWidgetProvider.getFeatureIdPref(appWidgetId), -1)
         val title = TrackAndGraphDatabase.getInstance(this).trackAndGraphDatabaseDao.tryGetFeatureById(featureId)?.name
-        val remoteViews = TrackWidgetProvider.createRemoteViews(this, appWidgetId, title)
+        val remoteViews = TrackWidgetProvider.createRemoteViews(this, appWidgetId, title, disableWidget)
 
         appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
     }
