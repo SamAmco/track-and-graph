aa9cca7a807b6e00841215e0f9c870a1f36005b1
Samuel Harvey-Collins <samuel.amco@gmail.com>
2022-04-30 21:14:49 +0100

    Add hilt dependency

--- app/src/main/java/com/samco/trackandgraph/TrackAndGraphApplication.kt
+++ app/src/main/java/com/samco/trackandgraph/TrackAndGraphApplication.kt
@@ -18,8 +18,10 @@ package com.samco.trackandgraph
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class TrackAndGraphApplication : Application() {
     override fun onCreate() {
         super.onCreate()
