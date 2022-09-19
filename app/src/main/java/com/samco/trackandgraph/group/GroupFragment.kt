d365faf2d2ca72d55ac00642a01263667e23a615
Samuel Harvey-Collins <samuel.amco@gmail.com>
2022-09-19 12:51:29 +0100

    Temporarily hide add function menu option

--- app/src/main/java/com/samco/trackandgraph/group/GroupFragment.kt
+++ app/src/main/java/com/samco/trackandgraph/group/GroupFragment.kt
@@ -123,7 +123,7 @@ class GroupFragment : Fragment(), YesCancelDialogFragment.YesCancelDialogListene
                 R.id.add_group -> onAddGroupClicked()
                 R.id.export_button -> onExportClicked()
                 R.id.import_button -> onImportClicked()
-                R.id.add_function -> onAddFunctionClicked()
+                //R.id.add_function -> onAddFunctionClicked()
                 else -> return false
             }
             return true
