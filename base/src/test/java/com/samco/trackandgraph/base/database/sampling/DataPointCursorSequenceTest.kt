6f904283967726393e8ae8d585967e2c9f3cb415
Samuel Harvey-Collins <samuel.amco@gmail.com>
2022-09-22 19:23:35 +0100

    Revert to iterating data points with a cursor because it's faster

---
+++ base/src/test/java/com/samco/trackandgraph/base/database/sampling/DataPointCursorSequenceTest.kt
@@ -0,0 +1,47 @@
+package com.samco.trackandgraph.base.database.sampling
+
+import android.database.Cursor
+import com.nhaarman.mockitokotlin2.*
+import com.samco.trackandgraph.base.database.TrackAndGraphDatabaseDao
+import com.samco.trackandgraph.base.database.entity.DataPoint
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.threeten.bp.OffsetDateTime
+
+class DataPointCursorSequenceTest {
+    @Test
+    fun `Test sequence iterates the whole database correctly`() {
+        //PREPARE
+        val dao = mock<TrackAndGraphDatabaseDao>()
+        val featureId = 0L
+        val backingData = IntRange(0, 3).toList()
+        val cursor = mock<Cursor>()
+        var cursorPosition = 0
+        var value = 0
+        whenever(dao.getDataPointsCursor(any())).thenReturn(cursor)
+
+        whenever(cursor.position).thenAnswer { cursorPosition }
+        whenever(cursor.count).thenReturn(backingData.size)
+        whenever(cursor.moveToNext()).thenAnswer {
+            cursorPosition++
+            true
+        }
+        whenever(cursor.getString(eq(0))).thenReturn("2022-09-14T21:30:41.432+01:00")
+        whenever(cursor.getLong(eq(1))).thenReturn(featureId)
+        whenever(cursor.getDouble(eq(2))).thenAnswer { (value++).toDouble() }
+        whenever(cursor.getString(eq(3))).thenReturn("")
+        whenever(cursor.getString(eq(4))).thenReturn("")
+
+        val sequence = DataPointCursorSequence(cursor)
+
+        //EXECUTE
+        val output1 = sequence.iterator().asSequence().toList()
+        val output2 = sequence.iterator().asSequence().toList()
+
+        //VERIFY
+        assertEquals(backingData, output1.map { it.value.toInt() })
+        assertEquals(backingData, output2.map { it.value.toInt() })
+        verify(cursor, times(backingData.size)).getDouble(eq(2))
+
+    }
+}
