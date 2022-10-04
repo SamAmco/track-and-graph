14232304f0394aae89b2ad648429f1f849bfe1dd
Samuel Harvey-Collins <samuel.harvey-collins@conjure.co.uk>
2022-10-04 17:50:46 +0100

    Refactor add tracker view and add focus behaviour

---
+++ app/src/test/java/com/samco/trackandgraph/ui/compose/viewmodels/HelpersKtTest.kt
@@ -0,0 +1,35 @@
+package com.samco.trackandgraph.ui.compose.viewmodels
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+
+internal class HelpersKtTest {
+
+    @Test
+    fun asValidatedInt() {
+        assertEquals("-1", "-1-".asValidatedInt())
+        assertEquals("-1", "-1.0".asValidatedInt())
+        assertEquals("-1", "-1.0".asValidatedInt())
+        assertEquals("1", "1.1".asValidatedInt())
+        assertEquals("1001234", "1001234".asValidatedInt())
+        assertEquals("-1001234", "-1001234".asValidatedInt())
+        assertEquals("-", "-".asValidatedInt())
+        assertEquals("0", "0".asValidatedInt())
+    }
+
+    @Test
+    fun asValidatedDouble() {
+        assertEquals("-1.0", "-1.0".asValidatedDouble())
+        assertEquals("1.0", "1.0".asValidatedDouble())
+        assertEquals("812341.123", "812341.123".asValidatedDouble())
+        assertEquals("-812341.123", "-812341.123".asValidatedDouble())
+        assertEquals("1", "1".asValidatedDouble())
+        assertEquals("-1", "-1".asValidatedDouble())
+        assertEquals("-1", "-1-".asValidatedDouble())
+        assertEquals("-1.", "-1-.".asValidatedDouble())
+        assertEquals(".", ".".asValidatedDouble())
+        assertEquals("0.", "0.".asValidatedDouble())
+        assertEquals("-1.00", "-1.0.0".asValidatedDouble())
+    }
+}
