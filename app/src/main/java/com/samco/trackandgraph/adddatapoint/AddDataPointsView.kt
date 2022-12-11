2f133ee1db364c1354830c58adb966a165499583
Samuel Harvey-Collins <samuel.harvey-collins@conjure.co.uk>
2022-12-11 16:57:19 +0000

    Add note button

--- app/src/main/java/com/samco/trackandgraph/adddatapoint/AddDataPointsView.kt
+++ app/src/main/java/com/samco/trackandgraph/adddatapoint/AddDataPointsView.kt
@@ -20,12 +20,9 @@ package com.samco.trackandgraph.adddatapoint
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -33,18 +30,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.samco.trackandgraph.R
-import com.samco.trackandgraph.ui.compose.theming.disabledAlpha
 import com.samco.trackandgraph.ui.compose.theming.tngColors
 import com.samco.trackandgraph.ui.compose.ui.*
 import kotlinx.coroutines.flow.distinctUntilChanged
@@ -54,6 +47,7 @@ import org.threeten.bp.OffsetDateTime
 fun AddDataPointsView(viewModel: AddDataPointsViewModel) = Surface {
     Column(
         modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxWidth()
             .background(color = MaterialTheme.tngColors.surface)
             .padding(
@@ -151,7 +145,7 @@ private fun TrackerPage(viewModel: AddDataPointViewModel) =
             onDateTimeSelected = viewModel::updateTimestamp
         )
 
-        SpacingSmall()
+        SpacingLarge()
 
         when (viewModel) {
             is AddDataPointViewModel.NumericalDataPointViewModel -> {
@@ -188,30 +182,48 @@ private fun TrackerPage(viewModel: AddDataPointViewModel) =
                 focusManager = focusManager
             )
         }
-        
-        SpacingLarge()
+
+        SpacingSmall()
 
         NoteInput(viewModel)
-
-        SpacingLarge()
     }
 
 @Composable
-private fun NoteInput(viewModel: AddDataPointViewModel) {
-    val note by viewModel.note.observeAsState("")
-    var showNoteBox by rememberSaveable { mutableStateOf(true) }
+private fun NoteInput(viewModel: AddDataPointViewModel) =
+    Row(
+        modifier = Modifier.width(IntrinsicSize.Max),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        val note by viewModel.note.observeAsState("")
+        var showNoteBox by rememberSaveable { mutableStateOf(false) }
 
-    if (showNoteBox) {
-        FullWidthTextField(
-            value = note,
-            onValueChange = { viewModel.updateNote(it) },
-            label = stringResource(id = R.string.note_input_hint),
-            singleLine = false
-        )
-    } else {
-
+        if (showNoteBox) {
+            FullWidthTextField(
+                value = note,
+                onValueChange = { viewModel.updateNote(it) },
+                label = stringResource(id = R.string.note_input_hint),
+                singleLine = false
+            )
+        } else {
+            TextButton(
+                onClick = { showNoteBox = true },
+                colors = ButtonDefaults.textButtonColors(
+                    contentColor = MaterialTheme.tngColors.onSurface
+                )
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.edit_icon),
+                    contentDescription = stringResource(id = R.string.add_a_note)
+                )
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = stringResource(id = R.string.add_a_note),
+                    fontSize = MaterialTheme.typography.body1.fontSize,
+                    fontWeight = MaterialTheme.typography.body1.fontWeight,
+                )
+            }
+        }
     }
-}
 
 @Composable
 private fun HintHeader(viewModel: AddDataPointsViewModel) =
