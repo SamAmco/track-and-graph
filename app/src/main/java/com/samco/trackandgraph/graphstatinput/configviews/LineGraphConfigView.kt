0d5bdd2d62e86478d4ed0579a1cda5ee07a33ca0
Samuel Harvey-Collins <samuel.amco@gmail.com>
2023-02-23 18:25:39 +0000

    Refactor sample ending at input UI

--- app/src/main/java/com/samco/trackandgraph/graphstatinput/configviews/LineGraphConfigView.kt
+++ app/src/main/java/com/samco/trackandgraph/graphstatinput/configviews/LineGraphConfigView.kt
@@ -16,18 +16,10 @@
  */
 package com.samco.trackandgraph.graphstatinput.configviews
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringArrayResource
-import com.samco.trackandgraph.R
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.samco.trackandgraph.graphstatinput.customviews.GraphStatDurationSpinner
-import com.samco.trackandgraph.ui.compose.ui.LabeledRow
-import com.samco.trackandgraph.ui.compose.ui.TextMapSpinner
-import com.samco.trackandgraph.ui.compose.ui.showDateDialog
+import com.samco.trackandgraph.graphstatinput.customviews.GraphStatEndingAtSpinner
 import org.threeten.bp.OffsetDateTime
 
 enum class SampleEndingAtOption {
@@ -57,29 +49,9 @@ fun LineGraphConfigView(
         onDurationSelected = { viewModel.setDuration(it) }
     )
 
-    //TODO extract this into a composable
-    LabeledRow(label = stringResource(id = R.string.ending_at_colon)) {
-        val strings = stringArrayResource(id = R.array.ending_at_values)
-
-        val spinnerItems = mapOf(
-            SampleEndingAtOption.LATEST to strings[0],
-            SampleEndingAtOption.CUSTOM to strings[1]
-        )
-
-        val context = LocalContext.current
-        //TODO the spinner item should say the date not custom
-        TextMapSpinner(
-            modifier = Modifier.padding(0.dp),
-            strings = spinnerItems,
-            selectedItem = viewModel.sampleEndingAt.option,
-            onItemSelected = { option ->
-                when (option) {
-                    SampleEndingAtOption.LATEST -> viewModel.setSampleEnding(SampleEndingAt.Latest)
-                    SampleEndingAtOption.CUSTOM -> showDateDialog(context) {
-                        viewModel.setSampleEnding(SampleEndingAt.Custom(it))
-                    }
-                }
-            }
-        )
-    }
+    GraphStatEndingAtSpinner(
+        modifier,
+        viewModel.sampleEndingAt
+    ) { viewModel.setSampleEnding(it) }
 }
+
