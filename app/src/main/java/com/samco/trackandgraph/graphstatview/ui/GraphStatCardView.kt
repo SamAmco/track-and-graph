cd118974f4774281990c56b64293115ba8f86079
Samuel Harvey-Collins <samuel.amco@gmail.com>
2023-05-07 01:04:20 +0100

    Implement graph stat card click listener

--- app/src/main/java/com/samco/trackandgraph/graphstatview/ui/GraphStatCardView.kt
+++ app/src/main/java/com/samco/trackandgraph/graphstatview/ui/GraphStatCardView.kt
@@ -1,15 +1,18 @@
 package com.samco.trackandgraph.graphstatview.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.samco.trackandgraph.R
 import com.samco.trackandgraph.graphstatview.factories.viewdto.*
 import com.samco.trackandgraph.group.GraphStatClickListener
@@ -27,18 +30,29 @@ fun GraphStatCardView(
         .fillMaxWidth()
 ) {
     Card(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .let {
+                if (clickListener != null) it.clickable { clickListener.onClick(graphStatViewData) }
+                else it
+            },
         elevation = if (isElevated)
             dimensionResource(id = R.dimen.card_elevation) * 3f
         else dimensionResource(R.dimen.card_elevation)
     ) {
-        //TODO add a try catch around the graph views and show an error message if it fails
         Box(
             modifier = Modifier
                 .padding(dimensionResource(id = R.dimen.card_padding))
                 .fillMaxWidth()
         ) {
-            //TODO add menu button (only enable if clickListener is not null)
+            if (clickListener != null) {
+                MenuSection(
+                    modifier = Modifier.align(Alignment.TopEnd),
+                    clickListener = clickListener,
+                    graphStatViewData = graphStatViewData
+                )
+            }
+
             Column(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -58,6 +72,71 @@ fun GraphStatCardView(
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun MenuSection(
+    modifier: Modifier = Modifier,
+    clickListener: GraphStatClickListener,
+    graphStatViewData: IGraphStatViewData
+) = Box(modifier = modifier) {
+
+    var expanded by remember { mutableStateOf(false) }
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Icon(
+        modifier = modifier.clickable(
+            interactionSource = interactionSource,
+            indication = null
+        ) { expanded = true },
+        painter = painterResource(id = R.drawable.list_menu_icon),
+        contentDescription = null
+    )
+
+    DropdownMenu(
+        modifier = Modifier
+            .widthIn(min = 180.dp)
+            .background(MaterialTheme.colors.background),
+        expanded = expanded,
+        onDismissRequest = { expanded = false }
+    ) {
+        DropdownMenuItem(onClick = {
+            clickListener.onDelete(graphStat = graphStatViewData)
+            expanded = false
+        }) {
+            Text(
+                text = stringResource(id = R.string.delete),
+                style = MaterialTheme.typography.body1
+            )
+        }
+        DropdownMenuItem(onClick = {
+            clickListener.onEdit(graphStat = graphStatViewData)
+            expanded = false
+        }) {
+            Text(
+                text = stringResource(id = R.string.edit),
+                style = MaterialTheme.typography.body1
+            )
+        }
+        DropdownMenuItem(onClick = {
+            clickListener.onMoveGraphStat(graphStat = graphStatViewData)
+            expanded = false
+        }) {
+            Text(
+                text = stringResource(id = R.string.move_to),
+                style = MaterialTheme.typography.body1
+            )
+        }
+        DropdownMenuItem(onClick = {
+            clickListener.onDuplicate(graphStat = graphStatViewData)
+            expanded = false
+        }) {
+            Text(
+                text = stringResource(id = R.string.duplicate),
+                style = MaterialTheme.typography.body1
+            )
         }
     }
 }
