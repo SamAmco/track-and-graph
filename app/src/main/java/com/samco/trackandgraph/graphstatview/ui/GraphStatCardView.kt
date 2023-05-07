78babb3a0f06010f9baa0cfc7e74ac5d8f1d8b35
Samuel Harvey-Collins <samuel.amco@gmail.com>
2023-05-07 01:53:39 +0100

    Fix bug long press ripple highlight doesn't fade for graph views

--- app/src/main/java/com/samco/trackandgraph/graphstatview/ui/GraphStatCardView.kt
+++ app/src/main/java/com/samco/trackandgraph/graphstatview/ui/GraphStatCardView.kt
@@ -1,13 +1,18 @@
 package com.samco.trackandgraph.graphstatview.ui
 
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -17,6 +22,48 @@ import com.samco.trackandgraph.R
 import com.samco.trackandgraph.graphstatview.factories.viewdto.*
 import com.samco.trackandgraph.group.GraphStatClickListener
 import com.samco.trackandgraph.ui.compose.ui.SpacingSmall
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+
+
+//This fixes a bug where the card would show a ripple effect when the user long pressed on the card
+// but not remove the ripple effect when the user released the long press. I assume this is because
+// we are wrapping composable interaction code in recycler view item touch helper code and item
+// touch helper does not effectively cancel the interaction when the user goes into long press/dragging
+// mode.
+@Stable
+private class AutoCancelMutableInteractionSourceImpl : MutableInteractionSource {
+    private val interactionsInput = MutableSharedFlow<Interaction>(
+        extraBufferCapacity = 16,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST,
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val interactions: Flow<Interaction> = interactionsInput
+        .flatMapLatest {
+            flow {
+                emit(it)
+                if (it is PressInteraction.Press) {
+                    delay(1000)
+                    emit(PressInteraction.Release(PressInteraction.Press(Offset(0f, 0f))))
+                }
+            }
+        }
+
+
+    override suspend fun emit(interaction: Interaction) {
+        interactionsInput.emit(interaction)
+    }
+
+    override fun tryEmit(interaction: Interaction): Boolean {
+        return interactionsInput.tryEmit(interaction)
+    }
+}
 
 @Composable
 fun GraphStatCardView(
@@ -33,8 +80,12 @@ fun GraphStatCardView(
         modifier = modifier
             .fillMaxWidth()
             .let {
-                if (clickListener != null) it.clickable { clickListener.onClick(graphStatViewData) }
-                else it
+                if (clickListener != null) {
+                    it.clickable(
+                        indication = LocalIndication.current,
+                        interactionSource = AutoCancelMutableInteractionSourceImpl()
+                    ) { clickListener.onClick(graphStatViewData) }
+                } else it
             },
         elevation = if (isElevated)
             dimensionResource(id = R.dimen.card_elevation) * 3f
