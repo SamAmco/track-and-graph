5d9b57daee512096d7e914fd891d8a5234082ec7
Propellant Dyke <propellant.dyke@gmail.com>
2023-12-11 23:16:33 +0100

    Change checkbox to switch in AddTracker form

---
+++ app/src/main/java/com/samco/trackandgraph/ui/compose/ui/RowSwitch.kt
@@ -0,0 +1,50 @@
+/*
+ *  This file is part of Track & Graph
+ *
+ *  Track & Graph is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Track & Graph is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Track & Graph.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.samco.trackandgraph.ui.compose.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RowSwitch(
+    checked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    text: String
+) = Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier
+        .fillMaxWidth()
+        .clickable { onCheckedChange?.invoke(!checked) }
+        .padding(end = 14.dp)
+) {
+    Text(
+        text = text,
+        modifier = Modifier.weight(1F)
+    )
+    Switch(
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+    )
+}
