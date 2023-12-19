2be33f2f15e170fb3197d108f4cb0e75f632fdc8
Samuel Harvey-Collins <samuel.amco@gmail.com>
2023-12-19 13:33:46 +0000

    Generate screenshots data

--- base/src/androidTest/java/com/samco/trackandgraph/DemoDBGenerator.kt
+++ base/src/androidTest/java/com/samco/trackandgraph/DemoDBGenerator.kt
@@ -22,7 +22,9 @@ import androidx.room.Room
 import androidx.sqlite.db.SimpleSQLiteQuery
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.samco.trackandgraph.base.database.TrackAndGraphDatabase
+import com.samco.trackandgraph.base.database.dto.DataPoint
 import com.samco.trackandgraph.base.database.dto.DataType
 import com.samco.trackandgraph.base.database.dto.Group
 import com.samco.trackandgraph.base.database.dto.Tracker
@@ -43,13 +45,20 @@ import com.samco.trackandgraph.base.system.SystemInfoProviderImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.threeten.bp.Duration
+import org.threeten.bp.OffsetDateTime
 import java.io.File
+import kotlin.math.roundToInt
+import kotlin.math.sin
+import kotlin.random.Random
 
 @RunWith(AndroidJUnit4::class)
 class DemoDBGenerator {
+
+    companion object {
+    }
 
     private val mockServiceManager = object : ServiceManager {
         override fun startTimerNotificationService() {
@@ -107,6 +116,7 @@ class DemoDBGenerator {
 
     @Before
     fun setup() {
+        AndroidThreeTen.init(InstrumentationRegistry.getInstrumentation().targetContext)
         database.openHelper.writableDatabase.execSQL(
             """
                 INSERT OR REPLACE INTO 
@@ -118,150 +128,10 @@ class DemoDBGenerator {
 
     @Test
     fun generateDemoDB(): Unit = runBlocking {
-        createOuterGroups()
-        createDailyGroup()
-
+        createScreenshotsGroup(dataInteractor)
         writeDBToFile()
     }
 
-    private suspend fun createDailyGroup() {
-        val dailyGroupId = dataInteractor.insertGroup(
-            createGroup(
-                name = "Daily",
-                displayIndex = 0,
-                colorIndex = 1
-            )
-        )
-
-        val sleepTracker = dataInteractor.insertTracker(
-            createTracker(
-                name = "Sleep",
-                groupId = dailyGroupId,
-                dataType = DataType.DURATION,
-                displayIndex = 0
-            )
-        )
-
-        val productivityTracker = dataInteractor.insertTracker(
-            createTracker(
-                name = "Productivity",
-                groupId = dailyGroupId,
-                displayIndex = 1
-            )
-        )
-
-        val alcoholTracker = dataInteractor.insertTracker(
-            createTracker(
-                name = "Alcohol",
-                groupId = dailyGroupId,
-                displayIndex = 2
-            )
-        )
-
-        val meditationTracker = dataInteractor.insertTracker(
-            createTracker(
-                name = "Meditation",
-                groupId = dailyGroupId,
-                dataType = DataType.DURATION,
-                displayIndex = 3
-            )
-        )
-
-        val workTracker = dataInteractor.insertTracker(
-            createTracker(
-                name = "Work",
-                groupId = dailyGroupId,
-                dataType = DataType.DURATION,
-                displayIndex = 4
-            )
-        )
-
-        val weightTracker = dataInteractor.insertTracker(
-            createTracker(
-                name = "Weight",
-                groupId = dailyGroupId,
-                displayIndex = 5
-            )
-        )
-
-        val exerciseTracker = dataInteractor.insertTracker(
-            createTracker(
-                name = "Exercise",
-                groupId = dailyGroupId,
-                hasDefaultValue = true,
-                defaultValue = 1.0,
-                displayIndex = 6
-            )
-        )
-
-        val studyingTracker = dataInteractor.insertTracker(
-            createTracker(
-                name = "Studying",
-                groupId = dailyGroupId,
-                dataType = DataType.DURATION,
-                displayIndex = 7
-            )
-        )
-    }
-
-    private suspend fun createOuterGroups() {
-        dataInteractor.insertGroup(
-            createGroup(
-                name = "Meal time tracking",
-                displayIndex = 1,
-                colorIndex = 11
-            )
-        )
-        dataInteractor.insertGroup(
-            createGroup(
-                name = "Morning tracking",
-                displayIndex = 2,
-                colorIndex = 6
-            )
-        )
-        dataInteractor.insertGroup(
-            createGroup(
-                name = "Daily tracking",
-                displayIndex = 3,
-                colorIndex = 0
-            )
-        )
-        dataInteractor.insertGroup(
-            createGroup(
-                name = "Weekly tracking",
-                displayIndex = 4,
-                colorIndex = 2
-            )
-        )
-        dataInteractor.insertGroup(
-            createGroup(
-                name = "Exercise routine tracking",
-                displayIndex = 5,
-                colorIndex = 8
-            )
-        )
-        dataInteractor.insertGroup(
-            createGroup(
-                name = "Weight loss graphs",
-                displayIndex = 6,
-                colorIndex = 7
-            )
-        )
-        dataInteractor.insertGroup(
-            createGroup(
-                name = "Mood quality",
-                displayIndex = 7,
-                colorIndex = 3
-            )
-        )
-        dataInteractor.insertGroup(
-            createGroup(
-                name = "Stress and rest statistics",
-                displayIndex = 8,
-                colorIndex = 4
-            )
-        )
-    }
 
     private fun writeDBToFile() {
         database.openHelper.writableDatabase.query(SimpleSQLiteQuery("PRAGMA wal_checkpoint(full)"))
@@ -285,43 +155,130 @@ class DemoDBGenerator {
         dbFile.copyTo(outFile, overwrite = true)
     }
 
-
-    private fun createGroup(
-        name: String = "",
-        displayIndex: Int = 0,
-        parentGroupId: Long? = null,
-        colorIndex: Int = 0,
-    ) = Group(
-        id = 0L,
-        name = name,
-        displayIndex = displayIndex,
-        parentGroupId = parentGroupId ?: 0L,
-        colorIndex = colorIndex
-    )
-
-    private fun createTracker(
-        name: String,
-        groupId: Long,
-        displayIndex: Int = 0,
-        description: String = "",
-        dataType: DataType = DataType.CONTINUOUS,
-        hasDefaultValue: Boolean = false,
-        defaultValue: Double = 0.0,
-        defaultLabel: String = "",
-        suggestionType: TrackerSuggestionType = TrackerSuggestionType.VALUE_AND_LABEL,
-        suggestionOrder: TrackerSuggestionOrder = TrackerSuggestionOrder.VALUE_ASCENDING,
-    ) = Tracker(
-        id = 0L,
-        name = name,
-        groupId = groupId,
-        featureId = 0L,
-        displayIndex = displayIndex,
-        description = description,
-        dataType = dataType,
-        hasDefaultValue = hasDefaultValue,
-        defaultValue = defaultValue,
-        defaultLabel = defaultLabel,
-        suggestionType = suggestionType,
-        suggestionOrder = suggestionOrder
-    )
 }
+
+const val SEC_PER_HOUR = 60.0 * 60.0
+
+class SinTransform(
+    val amplitude: Double,
+    val wavelength: Double,
+    val yOffset: Double = 0.0,
+    val xOffset: Double = 0.0
+) {
+    fun transform(index: Int): Double {
+        val xPos = index.toDouble() + xOffset
+        val sinTransform = sin((xPos / wavelength) * Math.PI * 2.0)
+        return (((sinTransform + 1.0) / 2.0) * amplitude) + yOffset
+    }
+}
+
+suspend fun createWaveData(
+    dataInteractor: DataInteractor,
+    trackerId: Long,
+    sinTransform: SinTransform = SinTransform(10.0, 3.0),
+    randomSeed: Int = 0,
+    randomOffsetScalar: Double = 5.0,
+    numDataPoints: Int = 1000,
+    spacing: Duration = Duration.ofDays(1),
+    spacingRandomisationHours: Int = 6,
+    endPoint: OffsetDateTime = OffsetDateTime.now(),
+    roundToInt: Boolean = false,
+    clampMin: Double? = null,
+    clampMax: Double? = null,
+    labels: List<String> = emptyList(),
+) {
+    val tracker =
+        dataInteractor.getTrackerById(trackerId) ?: throw Exception("Tracker not found")
+    val featureId = tracker.featureId
+
+    val random = Random(randomSeed)
+
+    for (i in 0 until numDataPoints) {
+        val sin = sinTransform.transform(i)
+        val randAdjusted = sin + (random.nextDouble() * randomOffsetScalar)
+        val rounded = if (roundToInt) randAdjusted.roundToInt().toDouble() else randAdjusted
+        val clamped = clamp(
+            value = rounded,
+            min = clampMin ?: Double.MIN_VALUE,
+            max = clampMax ?: Double.MAX_VALUE
+        )
+
+        val randDuration = Duration.ofHours(
+            random.nextLong(spacingRandomisationHours * 2L) - spacingRandomisationHours
+        )
+
+        val time = endPoint - spacing.multipliedBy(i.toLong()) - randDuration
+
+        val labelIndex = labels.getOrNull(clamped.roundToInt()) ?: ""
+
+        val dataPoint = createDataPoint(
+            timestamp = time,
+            featureId = featureId,
+            value = clamped,
+            label = labelIndex
+        )
+        dataInteractor.insertDataPoint(dataPoint)
+    }
+}
+
+fun clamp(value: Double, min: Double, max: Double): Double {
+    if (value < min) {
+        return min
+    } else if (value > max) {
+        return max
+    }
+    return value
+}
+
+fun createGroup(
+    name: String = "",
+    displayIndex: Int = 0,
+    parentGroupId: Long? = null,
+    colorIndex: Int = 0,
+) = Group(
+    id = 0L,
+    name = name,
+    displayIndex = displayIndex,
+    parentGroupId = parentGroupId ?: 0L,
+    colorIndex = colorIndex
+)
+
+fun createTracker(
+    name: String,
+    groupId: Long,
+    displayIndex: Int = 0,
+    description: String = "",
+    dataType: DataType = DataType.CONTINUOUS,
+    hasDefaultValue: Boolean = false,
+    defaultValue: Double = 0.0,
+    defaultLabel: String = "",
+    suggestionType: TrackerSuggestionType = TrackerSuggestionType.VALUE_AND_LABEL,
+    suggestionOrder: TrackerSuggestionOrder = TrackerSuggestionOrder.VALUE_ASCENDING,
+) = Tracker(
+    id = 0L,
+    name = name,
+    groupId = groupId,
+    featureId = 0L,
+    displayIndex = displayIndex,
+    description = description,
+    dataType = dataType,
+    hasDefaultValue = hasDefaultValue,
+    defaultValue = defaultValue,
+    defaultLabel = defaultLabel,
+    suggestionType = suggestionType,
+    suggestionOrder = suggestionOrder
+)
+
+fun createDataPoint(
+    timestamp: OffsetDateTime = OffsetDateTime.now(),
+    featureId: Long,
+    value: Double,
+    label: String = "",
+    note: String = ""
+) = DataPoint(
+    timestamp = timestamp,
+    featureId = featureId,
+    value = value,
+    label = label,
+    note = note
+)
