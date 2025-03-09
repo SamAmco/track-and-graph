fc1cd9373ae1bf5ef932463cb89943148a1dd99f
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-03-09 17:42:59 +0000

    Add lua icon to lua graph headers

--- app/src/main/java/com/samco/trackandgraph/graphstatview/ui/GraphStatView.kt
+++ app/src/main/java/com/samco/trackandgraph/graphstatview/ui/GraphStatView.kt
@@ -20,17 +20,27 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.samco.trackandgraph.R
 import com.samco.trackandgraph.base.database.dto.GraphStatType
 import com.samco.trackandgraph.graphstatview.GraphStatInitException
@@ -58,12 +68,10 @@ fun GraphStatView(
     modifier = modifier.fillMaxWidth(),
     horizontalAlignment = Alignment.CenterHorizontally
 ) {
-    Text(
-        text = graphStatViewData.graphOrStat.name,
-        style = MaterialTheme.typography.h6,
-        textAlign = TextAlign.Center
-    )
+    GraphHeading(graphStatViewData)
+
     DialogInputSpacing()
+
     when (graphStatViewData.state) {
         IGraphStatViewData.State.LOADING -> {
             Box(
@@ -83,6 +91,49 @@ fun GraphStatView(
         )
     }
 }
+
+@Composable
+private fun GraphHeading(
+    graphStatViewData: IGraphStatViewData,
+) {
+    if (graphStatViewData is ILuaGraphViewData) {
+        val annotatedString = remember(graphStatViewData) {
+            buildAnnotatedString {
+                appendInlineContent("lua_icon")
+                append(" ")
+                append(graphStatViewData.graphOrStat.name)
+            }
+        }
+        val iconSize = dimensionResource(id = R.dimen.icon_size_small).value.sp
+        val inlineContentMap = mapOf(
+            "lua_icon" to InlineTextContent(
+                placeholder = Placeholder(iconSize, iconSize, PlaceholderVerticalAlign.TextCenter),
+                children = { LuaIcon() }
+            )
+        )
+        Text(
+            text = annotatedString,
+            style = MaterialTheme.typography.h6,
+            inlineContent = inlineContentMap,
+            textAlign = TextAlign.Center
+        )
+    } else {
+        Text(
+            text = graphStatViewData.graphOrStat.name,
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+private fun LuaIcon(modifier: Modifier = Modifier) = Icon(
+    modifier = modifier
+        .size(dimensionResource(id = R.dimen.icon_size_small)),
+    painter = painterResource(R.drawable.lua_icon),
+    tint = MaterialTheme.colors.onSurface.copy(alpha = 0.5f),
+    contentDescription = null
+)
 
 @Composable
 private fun GraphError(
@@ -138,30 +189,52 @@ private fun GraphStatInnerViewOrLuaGraph(
     timeMarker: OffsetDateTime? = null,
     graphHeight: Int? = null
 ) {
-    var unwrapped = graphStatViewData
-
     if (graphStatViewData is ILuaGraphViewData) {
-        if (!graphStatViewData.hasData) {
-            GraphErrorView(
-                modifier = modifier,
-                error = R.string.graph_stat_view_not_enough_data_graph
-            )
-            return
-        }
-        unwrapped = graphStatViewData.wrapped ?: return
+        UnwrappedLuaGraphView(
+            modifier = modifier,
+            graphStatViewData = graphStatViewData,
+            listMode = listMode,
+            timeMarker = timeMarker,
+            graphHeight = graphHeight
+        )
+    } else {
+        GraphStatInnerView(
+            graphStatViewData = graphStatViewData,
+            listMode = listMode,
+            timeMarker = timeMarker,
+            graphHeight = graphHeight
+        )
     }
+}
 
-    if (unwrapped is ITextViewData) {
-        LuaTextView(textData = unwrapped)
+@Composable
+private fun UnwrappedLuaGraphView(
+    modifier: Modifier,
+    graphStatViewData: ILuaGraphViewData,
+    listMode: Boolean,
+    timeMarker: OffsetDateTime? = null,
+    graphHeight: Int? = null
+) {
+    if (!graphStatViewData.hasData) {
+        GraphErrorView(
+            modifier = modifier,
+            error = R.string.graph_stat_view_not_enough_data_graph
+        )
         return
     }
 
-    GraphStatInnerView(
-        graphStatViewData = unwrapped,
-        listMode = listMode,
-        timeMarker = timeMarker,
-        graphHeight = graphHeight
-    )
+    val unwrapped = graphStatViewData.wrapped ?: return
+
+    if (unwrapped is ITextViewData) {
+        LuaTextView(textData = unwrapped)
+    } else {
+        GraphStatInnerView(
+            graphStatViewData = unwrapped,
+            listMode = listMode,
+            timeMarker = timeMarker,
+            graphHeight = graphHeight
+        )
+    }
 }
 
 @Composable
