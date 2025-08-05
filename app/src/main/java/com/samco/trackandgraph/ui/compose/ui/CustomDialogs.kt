d5fc96275bb28cc9cb551db15bdd1d4db760db90
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-08-05 22:49:59 +0100

    Improvements to dialog code and fix for update where to dialog clipping in feature history screen

--- app/src/main/java/com/samco/trackandgraph/ui/compose/ui/CustomDialogs.kt
+++ app/src/main/java/com/samco/trackandgraph/ui/compose/ui/CustomDialogs.kt
@@ -23,8 +23,10 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.AlertDialog
@@ -36,6 +38,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.samco.trackandgraph.R
@@ -56,9 +59,12 @@ fun CustomDialog(
         onDismissRequest = onDismissRequest,
         properties = DialogProperties(
             usePlatformDefaultWidth = usePlatformDefaultWidth,
+            decorFitsSystemWindows = false
         )
     ) {
-        Surface {
+        Surface(
+            modifier = Modifier.imePadding()
+        ) {
             Column(
                 modifier = Modifier
                     .padding(paddingValues)
@@ -93,13 +99,21 @@ fun CustomConfirmCancelDialog(
     )
 ) {
     Column(
-        if (customWidthPercentage != null) {
-            Modifier.fillMaxWidth(customWidthPercentage)
-        } else Modifier
-            .wrapContentHeight()
-            .verticalScroll(rememberScrollState())
+        modifier = Modifier
+            .let {
+                if (customWidthPercentage != null) {
+                    it.fillMaxWidth(customWidthPercentage)
+                } else {
+                    it.wrapContentWidth()
+                }
+            }
     ) {
-        content()
+        Column(
+            modifier = Modifier
+                .weight(1f, fill = false)
+                .verticalScroll(rememberScrollState()),
+            content = content
+        )
 
         DialogInputSpacing()
 
