2864d83f9b3acc35f635f746abb2e5bd63e5aa53
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-08-09 19:11:25 +0100

    Refactor reminders view model to use StateFlow instead of LiveData

--- app/src/main/java/com/samco/trackandgraph/reminders/RemindersFragment.kt
+++ app/src/main/java/com/samco/trackandgraph/reminders/RemindersFragment.kt
@@ -24,20 +24,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.samco.trackandgraph.R
-import com.samco.trackandgraph.base.database.dto.CheckedDays
-import com.samco.trackandgraph.base.database.dto.Reminder
-import com.samco.trackandgraph.base.model.DataInteractor
-import com.samco.trackandgraph.base.model.di.IODispatcher
-import com.samco.trackandgraph.base.model.di.MainDispatcher
 import com.samco.trackandgraph.databinding.RemindersFragmentBinding
 import com.samco.trackandgraph.main.AppBarViewModel
 import com.samco.trackandgraph.permissions.PermissionRequesterUseCase
@@ -46,22 +41,18 @@ import com.samco.trackandgraph.util.bindingForViewLifecycle
 import com.samco.trackandgraph.util.hideKeyboard
 import com.samco.trackandgraph.util.resumeScoped
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.threeten.bp.LocalTime
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class RemindersFragment : Fragment(),
     PermissionRequesterUseCase by PermissionRequesterUseCaseImpl() {
     private var binding: RemindersFragmentBinding by bindingForViewLifecycle()
-    private val viewModel by viewModels<RemindersViewModel>()
+    private val viewModel: RemindersViewModel by viewModels<RemindersViewModelImpl>()
     private val appBarViewModel by activityViewModels<AppBarViewModel>()
     private lateinit var adapter: ReminderListAdapter
 
-    init { initNotificationsPermissionRequester(this) }
+    init {
+        initNotificationsPermissionRequester(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -87,6 +78,10 @@ class RemindersFragment : Fragment(),
         binding.remindersList.itemAnimator = DefaultItemAnimator()
         registerForContextMenu(binding.remindersList)
 
+        observeSaveChangesButton()
+        observeLoading()
+        observeAndUpdateReminders()
+
         return binding.root
     }
 
@@ -111,23 +106,24 @@ class RemindersFragment : Fragment(),
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        observeAndUpdateReminders()
-        observeSaveChangesButton()
-        observeLoading()
-    }
-
     private fun observeLoading() {
-        viewModel.loading.observe(viewLifecycleOwner) {
-            binding.loadingOverlay.visibility = if (it) View.VISIBLE else View.GONE
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.loading.collect {
+                    binding.loadingOverlay.visibility = if (it) View.VISIBLE else View.GONE
+                }
+            }
         }
     }
 
     private fun observeSaveChangesButton() {
         binding.saveChangesButton.setOnClickListener { viewModel.saveChanges() }
-        viewModel.remindersChanged.observe(viewLifecycleOwner) {
-            binding.saveChangesButton.visibility = if (it) View.VISIBLE else View.GONE
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.remindersChanged.collect {
+                    binding.saveChangesButton.visibility = if (it) View.VISIBLE else View.GONE
+                }
+            }
         }
     }
 
@@ -170,144 +166,26 @@ class RemindersFragment : Fragment(),
     }
 
     private fun observeAndUpdateReminders() {
-        viewModel.currentReminders.observe(viewLifecycleOwner) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.currentReminders.collect {
+                    if (it.isEmpty()) binding.noRemindersHintText.visibility = View.VISIBLE
+                    else binding.noRemindersHintText.visibility = View.GONE
 
-            if (it.isEmpty()) binding.noRemindersHintText.visibility = View.VISIBLE
-            else binding.noRemindersHintText.visibility = View.GONE
-
-            adapter.submitList(it)
+                    adapter.submitList(it)
+                }
+            }
         }
     }
 
     private fun onAddClicked() {
         viewModel.addReminder(getString(R.string.default_reminder_name))
-        viewModel.currentReminders.value?.size?.let {
-            binding.remindersList.smoothScrollToPosition(it)
-        }
+        binding.remindersList.smoothScrollToPosition(viewModel.currentReminders.value.size)
         requestAlarmAndNotificationPermission(requireContext())
     }
 
     override fun onPause() {
         onHideKeyboard()
         super.onPause()
-    }
-}
-
-@HiltViewModel
-class RemindersViewModel @Inject constructor(
-    private val dataInteractor: DataInteractor,
-    private val alarmInteractor: AlarmInteractor,
-    @IODispatcher private val io: CoroutineDispatcher,
-    @MainDispatcher private val ui: CoroutineDispatcher
-) : ViewModel() {
-
-    private val _currentReminders = MutableLiveData<List<Reminder>>(emptyList())
-    val currentReminders: LiveData<List<Reminder>> = _currentReminders
-
-    private var savedReminders: List<Reminder> = emptyList()
-
-    private val _remindersChanged = MutableLiveData(false)
-    private val _loading = MutableLiveData(true)
-
-    val remindersChanged: LiveData<Boolean> = _remindersChanged
-    val loading: LiveData<Boolean> = _loading
-
-    init {
-        viewModelScope.launch(io) {
-            withContext(ui) { _loading.value = true }
-            savedReminders = dataInteractor.getAllRemindersSync()
-            val current = savedReminders.toMutableList() //Copy the list
-            withContext(ui) {
-                _currentReminders.value = current
-                _loading.value = false
-            }
-        }
-    }
-
-    fun saveChanges() {
-        viewModelScope.launch(io) {
-            withContext(ui) { _loading.value = true }
-            _currentReminders.value?.let {
-                val withDisplayIndices = it
-                    .mapIndexed { index, reminder -> reminder.copy(displayIndex = index) }
-                alarmInteractor.clearAlarms()
-                dataInteractor.updateReminders(withDisplayIndices)
-                alarmInteractor.syncAlarms()
-                savedReminders = dataInteractor.getAllRemindersSync()
-                withContext(ui) {
-                    _currentReminders.value = savedReminders.toMutableList()
-                    onRemindersUpdated()
-                }
-            }
-            withContext(ui) { _loading.value = false }
-        }
-    }
-
-    private fun onRemindersUpdated() {
-        val a = savedReminders
-        val b = currentReminders.value ?: return
-        //If the two lists are not equal we have an update
-        _remindersChanged.value = !(a.size == b.size && a.zip(b).all { it.first == it.second })
-    }
-
-    fun addReminder(defaultName: String) {
-        _currentReminders.value = _currentReminders.value?.let {
-            val newReminder = Reminder(
-                //We just want a unique ID for now,
-                // this won't be used when it's added to the db
-                System.nanoTime(),
-                getNextDisplayIndex(),
-                defaultName,
-                LocalTime.now(),
-                CheckedDays.none()
-            )
-            it.toMutableList().apply { add(newReminder) }
-        }
-        onRemindersUpdated()
-    }
-
-    private fun getNextDisplayIndex(): Int {
-        return _currentReminders.value?.let {
-            if (it.isEmpty()) 0
-            else it.maxOf { r -> r.displayIndex } + 1
-        } ?: 0
-    }
-
-    fun deleteReminder(reminder: Reminder) {
-        _currentReminders.value = _currentReminders.value?.let { reminders ->
-            reminders.filter { it.id != reminder.id }
-        }
-        onRemindersUpdated()
-    }
-
-    fun adjustDisplayIndexes(indexUpdate: List<Reminder>) {
-        _currentReminders.value = _currentReminders.value?.let { curr ->
-            indexUpdate.mapIndexed { i, r ->
-                curr.firstOrNull { it.id == r.id }?.copy(displayIndex = i)
-            }.filterNotNull()
-        }
-        onRemindersUpdated()
-    }
-
-    fun daysChanged(reminder: Reminder, checkedDays: CheckedDays) =
-        updateReminder(reminder, reminder.copy(checkedDays = checkedDays))
-
-    fun onTimeChanged(reminder: Reminder, localTime: LocalTime) =
-        updateReminder(reminder, reminder.copy(time = localTime))
-
-    fun onNameChanged(reminder: Reminder, name: String) =
-        updateReminder(reminder, reminder.copy(alarmName = name))
-
-    private fun updateReminder(from: Reminder, to: Reminder) {
-        _currentReminders.value = _currentReminders.value?.let { reminders ->
-            val mutable = reminders.toMutableList()
-            val index = mutable.indexOfFirst { it.id == from.id }
-            if (index >= 0) {
-                mutable.removeAt(index)
-                mutable.add(index, to)
-            }
-            mutable
-        }
-        onRemindersUpdated()
     }
 }
