17d93eb4195bf3957e69b7abb89e48ae86a39799
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-08-22 23:11:19 +0100

    Fix graph stat input screen demo overlay padding issue

--- app/src/main/java/com/samco/trackandgraph/viewgraphstat/ViewGraphStatScreen.kt
+++ app/src/main/java/com/samco/trackandgraph/viewgraphstat/ViewGraphStatScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
