7b8342cfdb2e9a1b68bcc935d3b1ab668c2a70d7
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-08-29 16:39:30 +0100

    Add new database entities for functions, function steps, and funciton step input features

---
+++ data/src/main/java/com/samco/trackandgraph/data/database/entity/FunctionStep.kt
@@ -0,0 +1,47 @@
+/*
+ *  This file is part of Track & Graph
+ *
+ *  Track & Graph is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Track & Graph is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Track & Graph.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.samco.trackandgraph.data.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "function_steps_table",
+    foreignKeys = [ForeignKey(
+        entity = Function::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("function_id"),
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+internal data class FunctionStep(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id", index = true)
+    val id: Long,
+
+    @ColumnInfo(name = "function_id", index = true)
+    val functionId: Long,
+
+    @ColumnInfo(name = "step_index")
+    val stepIndex: Int,
+
+    @ColumnInfo(name = "script")
+    val script: String
+)
