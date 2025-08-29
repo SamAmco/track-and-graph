246a5306f7019ea7bb468e09d6066eb3196b00db
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-08-29 16:39:30 +0100

    Add new database entities for functions, function steps, and funciton step input features

---
+++ data/src/main/java/com/samco/trackandgraph/data/database/migrations/Migration56to57.kt
@@ -0,0 +1,101 @@
+/*
+ *  This file is part of Track & Graph
+ *
+ *  Track & Graph is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Track & Graph is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Track & Graph.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.samco.trackandgraph.data.database.migrations
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+val MIGRATION_56_57 = object : Migration(56, 57) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+
+        // Create functions_table
+        database.execSQL(
+            """
+                CREATE TABLE IF NOT EXISTS `functions_table` (
+                `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+                `feature_id` INTEGER NOT NULL, 
+                FOREIGN KEY(`feature_id`) REFERENCES `features_table`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE )
+            """.trimIndent()
+        )
+
+        // Create function_steps_table
+        database.execSQL(
+            """
+                CREATE TABLE IF NOT EXISTS `function_steps_table` (
+                `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+                `function_id` INTEGER NOT NULL, 
+                `step_index` INTEGER NOT NULL, 
+                `script` TEXT NOT NULL, 
+                FOREIGN KEY(`function_id`) REFERENCES `functions_table`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE )
+            """.trimIndent()
+        )
+
+        // Create function_step_input_features_table
+        database.execSQL(
+            """
+                CREATE TABLE IF NOT EXISTS `function_step_input_features_table` (
+                `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+                `function_step_id` INTEGER NOT NULL, 
+                `feature_id` INTEGER NOT NULL, 
+                `name` TEXT NOT NULL, 
+                FOREIGN KEY(`function_step_id`) REFERENCES `function_steps_table`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE , 
+                FOREIGN KEY(`feature_id`) REFERENCES `features_table`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE )
+            """.trimIndent()
+        )
+
+        // Create indexes for functions_table
+        database.execSQL(
+            """
+            CREATE INDEX IF NOT EXISTS `index_functions_table_id` ON functions_table (`id`)
+            """.trimIndent()
+        )
+        database.execSQL(
+            """
+            CREATE INDEX IF NOT EXISTS `index_functions_table_feature_id` ON functions_table (`feature_id`)
+            """.trimIndent()
+        )
+
+        // Create indexes for function_steps_table
+        database.execSQL(
+            """
+            CREATE INDEX IF NOT EXISTS `index_function_steps_table_id` ON function_steps_table (`id`)
+            """.trimIndent()
+        )
+        database.execSQL(
+            """
+            CREATE INDEX IF NOT EXISTS `index_function_steps_table_function_id` ON function_steps_table (`function_id`)
+            """.trimIndent()
+        )
+
+        // Create indexes for function_step_input_features_table
+        database.execSQL(
+            """
+            CREATE INDEX IF NOT EXISTS `index_function_step_input_features_table_id` ON function_step_input_features_table (`id`)
+            """.trimIndent()
+        )
+        database.execSQL(
+            """
+            CREATE INDEX IF NOT EXISTS `index_function_step_input_features_table_function_step_id` ON function_step_input_features_table (`function_step_id`)
+            """.trimIndent()
+        )
+        database.execSQL(
+            """
+            CREATE INDEX IF NOT EXISTS `index_function_step_input_features_table_feature_id` ON function_step_input_features_table (`feature_id`)
+            """.trimIndent()
+        )
+    }
+}
