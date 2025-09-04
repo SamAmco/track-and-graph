5f1f9b0b30a887cda27031ece50fd8d68cab4587
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-09-04 18:43:11 +0100

    Implement update function feature

--- app/src/main/java/com/samco/trackandgraph/main/NavigationHost.kt
+++ app/src/main/java/com/samco/trackandgraph/main/NavigationHost.kt
@@ -137,6 +137,9 @@ fun NavigationHost(
                     },
                     onAddFunction = { groupId ->
                         backStack.add(FunctionsNavKey(groupId = groupId))
+                    },
+                    onFunctionEdit = { displayFunction ->
+                        backStack.add(FunctionsNavKey(groupId = destination.groupId, functionId = displayFunction.id))
                     }
                 )
             }
