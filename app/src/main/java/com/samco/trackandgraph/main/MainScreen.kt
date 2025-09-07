832f3a0e73d50edf6730832e348d792fc90a7e21
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-09-07 12:49:26 +0100

    Attempt fix rare crashlytics app bar layout crash

--- app/src/main/java/com/samco/trackandgraph/main/MainScreen.kt
+++ app/src/main/java/com/samco/trackandgraph/main/MainScreen.kt
@@ -24,11 +24,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
@@ -242,7 +242,7 @@ fun AppBar(
 
     // We'll apply the status-bar inset INSIDE each slot
     val slotInset = Modifier
-        .fillMaxHeight()
+        .height(totalHeight)
         .windowInsetsPadding(WindowInsets.statusBars.only(WindowInsetsSides.Top))
 
     TopAppBar(
