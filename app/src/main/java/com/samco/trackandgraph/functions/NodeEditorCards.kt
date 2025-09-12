3227b65eaf71aede7f4c5091f756e8e070b49052
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2025-09-12 20:31:17 +0100

    Add arrow heads to edges in Node Editor

--- app/src/main/java/com/samco/trackandgraph/functions/NodeEditorCards.kt
+++ app/src/main/java/com/samco/trackandgraph/functions/NodeEditorCards.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.AwaitPointerEventScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
