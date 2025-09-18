4fd3ee04bf787c95612fb8ec197202bbb8b16a28
Sam <samuel.amco@gmail.com>
2025-09-18 22:14:45 +0100

    Bump version and changelog for v7.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         //If the backup file is not backwards compatible after this update, upgrade the major version number!
-        versionCode = 700004
-        versionName = "7.0.4"
+        versionCode = 700005
+        versionName = "7.0.5"
         testInstrumentationRunner = "com.samco.trackandgraph.screenshots.HiltTestRunner"
         // Default manifest placeholder for RecreateAlarms receiver
         manifestPlaceholders["recreateAlarmsEnabled"] = "true"
