7b142f2947fa2fbf945e022cdd92b349e518b748
SamAmco <samuel.amco@gmail.com>
2026-04-02 10:19:31 +0100

    Bump version and release notes for rc-v10.0.1

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         //If the backup file is not backwards compatible after this update, upgrade the major version number!
-        versionCode = 800010
-        versionName = "10.0.0"
+        versionCode = 800011
+        versionName = "10.0.1"
         testInstrumentationRunner = "com.samco.trackandgraph.screenshots.HiltTestRunner"
         // Default manifest placeholder for RecreateAlarms receiver
         manifestPlaceholders["recreateAlarmsEnabled"] = "true"
