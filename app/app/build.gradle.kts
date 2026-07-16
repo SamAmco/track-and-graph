6b7537e0f443561d4ae6a7352fb9b3dc5630a456
SamAmco <samuel.amco@gmail.com>
2026-07-16 22:54:42 +0100

    Bump v10.2.2 version code

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
     defaultConfig {
         applicationId = "com.samco.trackandgraph"
         //If the backup file is not backwards compatible after this update, upgrade the major version number!
-        versionCode = 800025
+        versionCode = 800026
         versionName = "10.2.2"
         // Default manifest placeholder for RecreateAlarms receiver
         manifestPlaceholders["recreateAlarmsEnabled"] = "true"
