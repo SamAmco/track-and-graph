1f1fb4dde54ed19a8776a996189e84e896d50a7a
SamAmco <samuel.amco@gmail.com>
2026-09-09 13:48:00 +0100

    Snapshot release 10.5.0-SNAPSHOT2

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
     defaultConfig {
         applicationId = "com.samco.trackandgraph"
         //If the backup file is not backwards compatible after this update, upgrade the major version number!
-        versionCode = 800031
-        versionName = "10.5.0-SNAPSHOT1"
+        versionCode = 800032
+        versionName = "10.5.0-SNAPSHOT2"
         // Default manifest placeholder for RecreateAlarms receiver
         manifestPlaceholders["recreateAlarmsEnabled"] = "true"
 
